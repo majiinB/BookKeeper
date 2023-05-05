ddf21d5880579414a318e1661a310ad63d01dac7
Arthur Artugue <artuguearthur@gmail.com>
2023-05-05 21:57:33 +0800

    added User and Employee

---
+++ Source code/src/com/bookkeeper/Employee.java
@@ -0,0 +1,54 @@
+package com.bookkeeper;
+
+public class Employee {
+	private int Employee_id;
+	private String Employee_name;
+	private String Position;
+	private String Email;
+	private String Employee_pass;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getEmployee_id() {
+		return Employee_id;
+	}
+
+	public void setEmployee_id(int employee_id) {
+		Employee_id = employee_id;
+	}
+
+	public String getEmployee_name() {
+		return Employee_name;
+	}
+
+	public void setEmployee_name(String employee_name) {
+		Employee_name = employee_name;
+	}
+
+	public String getPosition() {
+		return Position;
+	}
+
+	public void setPosition(String position) {
+		Position = position;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getEmployee_pass() {
+		return Employee_pass;
+	}
+
+	public void setEmployee_pass(String employee_pass) {
+		Employee_pass = employee_pass;
+	}
+
+}
---
+++ Source code/src/com/bookkeeper/User.java
@@ -0,0 +1,71 @@
+package com.bookkeeper;
+
+public class User {
+	private int User_id;
+	private String User_name;
+	private String User_email;
+	private String User_contact;
+	private String User_address;
+	private String User_pass;
+	
+	//Mutators and accessor 
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getUser_id() {
+		return User_id;
+	}
+
+	public void setUser_id(int user_id) {
+		User_id = user_id;
+	}
+
+	public String getUser_name() {
+		return User_name;
+	}
+
+	public void setUser_name(String user_name) {
+		User_name = user_name;
+	}
+
+	public String getUser_email() {
+		return User_email;
+	}
+
+	public void setUser_email(String user_email) {
+		User_email = user_email;
+	}
+
+	public String getUser_contact() {
+		return User_contact;
+	}
+
+	public void setUser_contact(String user_contact) {
+		User_contact = user_contact;
+	}
+
+	public String getUser_address() {
+		return User_address;
+	}
+
+	public void setUser_address(String user_address) {
+		User_address = user_address;
+	}
+
+	public String getUser_pass() {
+		return User_pass;
+	}
+
+	public void setUser_pass(String user_pass) {
+		User_pass = user_pass;
+	}
+	
+	//Method 
+	public void addUser() {
+		
+	}
+	public void disableUser() {
+		
+	}
+}
