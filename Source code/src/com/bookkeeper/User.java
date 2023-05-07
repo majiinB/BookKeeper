d99b62be332e31f17239069aee270a3a35e45cc8
Arthur Artugue <artuguearthur@gmail.com>
2023-05-07 21:45:22 +0800

    added encryption 🔒🔒🔒

--- Source code/src/com/bookkeeper/User.java
+++ Source code/src/com/bookkeeper/User.java
@@ -2,16 +2,18 @@ package com.bookkeeper;
 
 public class User {
 	private int User_id;
-	private String User_name;
+	private String User_fname;
+	private String User_lname;
 	private String User_email;
 	private String User_contact;
 	private String User_address;
 	private String User_pass;
 	
 	//Mutators and accessor 
-	public User(int User_id, String User_name, String User_email, String User_contact, String User_address, String User_pass) {
+	public User(int User_id, String User_fname,String User_lname, String User_email, String User_contact, String User_address, String User_pass) {
 		this.User_id = User_id;
-		this.User_name = User_name;
+		this.setUser_fname(User_fname);
+		this.setUser_lname(User_lname);
 		this.User_contact = User_contact;
 		this.User_address = User_address;
 		this.User_pass = User_pass;
@@ -23,14 +25,6 @@ public class User {
 
 	public void setUser_id(int user_id) {
 		User_id = user_id;
-	}
-
-	public String getUser_name() {
-		return User_name;
-	}
-
-	public void setUser_name(String user_name) {
-		User_name = user_name;
 	}
 
 	public String getUser_email() {
@@ -74,8 +68,24 @@ public class User {
 	}
 	
 	public String toString() {
-		String str = "User ID: " + User_id + "\nUser Name: " + User_name + "\nUser contact: " + User_contact + "\nUser address: " + User_address;
+		String str = "User ID: " + User_id + "\nUser Name: " + User_fname +" "+ User_lname + "\nUser contact: " + User_contact + "\nUser address: " + User_address;
 		return str;
 		
+	}
+
+	public String getUser_fname() {
+		return User_fname;
+	}
+
+	public void setUser_fname(String user_fname) {
+		User_fname = user_fname;
+	}
+
+	public String getUser_lname() {
+		return User_lname;
+	}
+
+	public void setUser_lname(String user_lname) {
+		User_lname = user_lname;
 	}
 }
--- Source code/src/com/bookkeeper/main.java
+++ Source code/src/com/bookkeeper/main.java
@@ -3,22 +3,58 @@ import java.sql.*;
 import java.util.Scanner;
 import java.awt.*;
 import javax.swing.*;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
 
 public class main {
 
 	public static void main(String[] args) {
 		
-		User newUser;
-		newUser = loginMethod();
+		User newUser = null;
+		try {
+			newUser = loginMethod();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(newUser.toString());
 		
-		String name = "John";
-		int age = 35;
-		String message = String.format("My name is %s and I am %d years old", name, age);
-		System.out.println(message);
 		
 		
     }
+	
+	public static String encryption(String pass) throws Exception{
+		String UserPass = pass;
+        String passphrase = "Book_KeeperSecretPassphrase";
+
+        byte[] iv = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+        byte[] key = Arrays.copyOf(passphrase.getBytes(StandardCharsets.UTF_8), 16);
+
+        // Create the AES cipher object
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+
+        // Encrypt the message
+        byte[] ciphertext = cipher.doFinal(UserPass.getBytes(StandardCharsets.UTF_8));
+
+        // Combine the initialization vector and the ciphertext
+        byte[] ivAndCiphertext = new byte[iv.length + ciphertext.length];
+        System.arraycopy(iv, 0, ivAndCiphertext, 0, iv.length);
+        System.arraycopy(ciphertext, 0, ivAndCiphertext, iv.length, ciphertext.length);
+
+        // Encode the result in base64
+        String encryptedMessage = Base64.getEncoder().encodeToString(ivAndCiphertext);
+		return encryptedMessage;
+	}
 	public void signUp() {
 		Connection conn = null;
 	    String url = "jdbc:mysql://localhost/library_management";
@@ -27,19 +63,33 @@ public class main {
 	    String username ="", pwd ="";
 	    Scanner scan = new Scanner(System.in);
 	    
+	    String userName;
+	    String userEmail;
+	    String userContact;
+	    String userAddress;
+	    String userPass;
+	    String userPassConfirm;
+	    String query;
 	    
+	    System.out.println("Enter User First Name: ");
+	    System.out.println("Enter User Last Name: ");
+	    System.out.println("Enter User name");
+	    System.out.println("Enter User Email");
+	    System.out.println("Enter User Address");
+	    System.out.println("Enter User Password");
+	    System.out.println("Confirm Password: ");  
 	}
 	//LoginMethod
-	public static User loginMethod(){
+	public static User loginMethod() throws Exception{
 		 //Declare variables
 		 int tries = 0;
 		 boolean loginCondition = true;
 		 boolean forReturn = false;
 		 Connection conn = null;
-	     String url = "jdbc:mysql://localhost/library_management";
+	     String url = "jdbc:mysql://localhost/book_keeper";
 	     String user = "root";
 	     String password = "";
-	     String username ="", pwd ="";
+	     String userEmail ="", pwd ="", encryptedpass = "";
 	     Scanner scan = new Scanner(System.in);
 	     
 	     //Loop for input verification with database connection exception handling
@@ -51,28 +101,29 @@ public class main {
 	   	         //System.out.println("Connected to the database");
 	   	         
 	   	         //User input
-	   	         System.out.print("Enter username: ");
-	   	         username = scan.nextLine();
+	   	         System.out.print("Enter User email: ");
+	   	         userEmail = scan.nextLine();
 	   	         System.out.print("Enter password: ");
 	   	         pwd = scan.nextLine();
-	   	         
+	   	         encryptedpass = encryption(pwd);
 	   	         //prepare query
-	   	         String query = "SELECT * FROM user WHERE BINARY User_name=? AND BINARY User_pass=?";
+	   	         String query = "SELECT * FROM patron WHERE BINARY patron_email=? AND BINARY patron_password=?";
 	   	         PreparedStatement stmt = conn.prepareStatement(query);
-	   	         stmt.setString(1, username);
-	   	         stmt.setString(2, pwd);
+	   	         stmt.setString(1, userEmail);
+	   	         stmt.setString(2, encryptedpass);
 	   	         ResultSet rs = stmt.executeQuery();
 	   	         
 	   	         //Condition for return and loop continuation
 	   	         if (rs.next()) {
 	   	             System.out.println("Login successful!");
-	   	             int userID = rs.getInt("user_id");
-	   	             String userName = rs.getString("User_name");
-	   	             String userEmail = rs.getString("User_email");
-	   	             String userContact = rs.getString("User_contact");
-	   	             String userAddress = rs.getString("User_address");
-	   	             String userPass = rs.getString("User_pass");
-	   	             User onlineUser = new User(userID, userName, userEmail, userContact, userAddress, userPass);
+	   	             int userID = rs.getInt("patron_id");
+	   	             String userFname = rs.getString("patron_fname");
+	   	             String userLname = rs.getString("patron_lname");
+	   	             String userEmail1 = rs.getString("patron_email");
+	   	             String userContact = rs.getString("patron_contact");
+	   	             String userAddress = rs.getString("patron_address");
+	   	             String userPass = rs.getString("patron_password");
+	   	             User onlineUser = new User(userID, userFname, userLname, userEmail1, userContact, userAddress, userPass);
 	   	         	 
 	   	             scan.close();
 	   	             conn.close();
