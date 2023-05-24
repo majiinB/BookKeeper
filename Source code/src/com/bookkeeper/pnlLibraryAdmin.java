edaab590b9412b92290661d482d539999d98e4fc
Arthur Artugue <artuguearthur@gmail.com>
2023-05-24 09:54:28 +0800

    added codes inside pnl Library Admin mouse event

--- Source code/src/com/bookkeeper/pnlLibraryAdmin.java
+++ Source code/src/com/bookkeeper/pnlLibraryAdmin.java
@@ -88,11 +88,35 @@ public class pnlLibraryAdmin extends JPanel {
         		frame.setVisible(true);  
             }
         });
+        
+        //Action listener for add book to show pop up
         JButton btnAddBook = new JButton("Add Book");
         btnAddBook.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		BookInfoFrame frame = new BookInfoFrame(1);
-        		frame.setVisible(true);
+        		int selectedRow = table.getSelectedRow();
+                if (selectedRow != -1) {
+                    // Get the values from the selected row
+                    int bookId = (int) table.getValueAt(selectedRow, 0);
+                    /*String bookTitle = (String) table.getValueAt(selectedRow, 1);
+                    String authorName = (String) table.getValueAt(selectedRow, 2);
+                    String genreName = (String) table.getValueAt(selectedRow, 3);
+                    String bookPublisher = (String) table.getValueAt(selectedRow, 4);
+                    String bookStatus = (String) table.getValueAt(selectedRow, 5);
+                    int aisleNumber = (int) table.getValueAt(selectedRow, 6);
+                    int shelfNumber = (int) table.getValueAt(selectedRow, 7);*/
+
+                    // Create a Book object with the retrieved values
+                    //Book selectedBook = new Book(bookId, bookTitle, authorName, genreName, bookPublisher, bookStatus, aisleNumber, shelfNumber);
+
+                    // Use the selectedBook object as needed
+                    // ...
+
+                    // Open the BookInfoFrame with the selected book
+                    //BookInfoFrame frame = new BookInfoFrame(selectedBook);
+                    //frame.setVisible(true);
+                    BookInfoFrame frame = new BookInfoFrame(1);
+                    frame.setVisible(true);
+                }
         	}
         });
         
@@ -124,7 +148,7 @@ public class pnlLibraryAdmin extends JPanel {
                     
                     // Check for empty search
                     if (getSearch.isEmpty()) {
-                        getQuery = "SELECT b.book_id, b.book_title, b.author_name, b.book_publisher, b.genre_name, b.book_status, l.aisle_number, l.shelf_number FROM book b " +
+                        getQuery = "SELECT b.book_id, b.book_title, b.author_name, b.genre_name, b.book_publisher,  b.book_status, l.aisle_number, l.shelf_number FROM book b " +
                                 "JOIN location l ON b.location_id = l.location_id ORDER BY book_title ASC;";
                     } else {
                         getQuery = searchQuery(getSearch);
@@ -185,7 +209,7 @@ public class pnlLibraryAdmin extends JPanel {
             // Establish database connection
         	//Rekta na kasi tinamad mag assign pa ng variables same lang naman kasi db na gagamitin HAHAHAHAA
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/book_keeper", "root", "");
-            String getQuery = "SELECT b.book_id, b.book_title, b.author_name, b.book_publisher, b.genre_name, b.book_status, l.aisle_number, l.shelf_number FROM book b " +
+            String getQuery = "SELECT b.book_id, b.book_title, b.author_name, b.genre_name,b.book_publisher,  b.book_status, l.aisle_number, l.shelf_number FROM book b " +
                     "JOIN location l ON b.location_id = l.location_id ORDER BY book_title ASC;";
 
             // Execute the SQL query
